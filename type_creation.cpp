e137432f5902e24e6bf72295fbb1642899f89919
Eduardo Amaral Melo <eduardoamelo2003@gmail.com>
2021-10-22 08:50:13 -0300

    creating types

---
+++ type_creation.cpp
@@ -0,0 +1,17 @@
+//Redefinir dados
+/*
+podemos criar o próprio tipo de variável
+*/
+#include <iostream>
+using namespace std;
+
+typedef float nota;
+
+int main() {
+	nota p1;
+	
+	cin >> p1;
+	cout << "nota = " << p1 < "\n";
+	
+	return 0;
+}
