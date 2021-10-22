cfe34b33d5d1526218dafce188db3b0d9fb3cf92
Eduardo Amaral Melo <eduardoamelo2003@gmail.com>
2021-10-22 09:05:27 -0300

    constants

---
+++ Constants.cpp
@@ -0,0 +1,14 @@
+//Constant definition
+/*
+#define NUM_ELEMENTS 
+	sempre depois do include
+*/
+#include <iostream>
+
+using namespace std;
+#define NOTA 10
+#define MENSAGEM "Parabens, nota "
+
+int main() {
+	cout << MENSAGEM << NOTA << endl;
+}
